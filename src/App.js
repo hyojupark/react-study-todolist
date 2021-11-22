11817c918cb6171d23dab2232b35c079d9098d5e
hyoju <hjpark@diteam.co.kr>
2021-11-22 20:51:30 +0900

    Todo 추가 기능 구현 및 연동

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Todo from './Todo';
-import { Paper, List } from '@material-ui/core';
+import AddTodo from './AddTodo';
+import { Paper, List, Container } from '@material-ui/core';
 import './App.css';
 
 class App extends React.Component {
@@ -12,6 +13,15 @@ class App extends React.Component {
                 {id: 1, title: "Hello World 1", done: false },
             ],
         };
+    }
+
+    add = (item) => {
+        const thisItems = this.state.items;
+        item.id = "ID-" + thisItems.length;
+        item.done = false;
+        thisItems.push(item);
+        this.setState({ items: thisItems });
+        console.log("item : ", this.state.items);
     }
 
 	render() {
@@ -29,9 +39,15 @@ class App extends React.Component {
         );
 
 		return (
-			<div className="App">
-				{todoItems}
-			</div>
+			// <div className="App">
+			// 	{todoItems}
+			// </div>
+            <div className="App">
+                <Container maxWidth="md">
+                    <AddTodo add={this.add} />
+                    <div className="TodoList">{todoItems}</div>
+                </Container>
+            </div>
 		)
 	}
 }
